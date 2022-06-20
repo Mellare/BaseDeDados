afb34580b4afbcb470933c553ffffa264faba955
Mellare <86076128+Mellare@users.noreply.github.com>
2022-06-19 21:36:28 -0300

    Update OperacaoManager.cs

--- Net6.1/BaseDeDados/src/Operacao/OperacaoManager.cs
+++ Net6.1/BaseDeDados/src/Operacao/OperacaoManager.cs
@@ -2,22 +2,41 @@ namespace BaseDeDados.Operacao;
 
 public class OperacaoManager
 {
+    public class OperacaoManager
+{
+Stack<string> pilha = new Stack<string>();
     public void AdicionarNovaOperacao()
     {
-        Console.WriteLine("Método Não Implementado (AINDA)");
+        Console.WriteLine("Digite a operação");
+        var operacao = Console.ReadLine();
+        pilha.Push(operacao);
+        Console.WriteLine("Operação adicionada com sucesso!");
         Console.ReadLine();
     }
 
     public void DesfazerUltimaOperacao()
     {
-        Console.WriteLine("Método Não Implementado (AINDA)");
-        Console.ReadLine();
+        if(pilha.Count <= 0){
+            Console.WriteLine("Nenhuma operação para ser desfeita");
+            Console.ReadLine();
+        }
+        else{
+            Console.WriteLine($"Ultima operação desfeita: {pilha.Pop()}");
+            Console.ReadLine();
+        }
     }
 
     public void RealizarTodasAsOperacaos()
     {
-        Console.WriteLine("Método Não Implementado (AINDA)");
+         if(pilha.Count <= 0){
+            Console.WriteLine("Nenhuma operação para ser realizada");
+        }
+        else{
+            while(pilha.Count() != 0)
+            {
+                Console.WriteLine($"{pilha.Pop()} realizada");
+            }
+        }
         Console.ReadLine();
     }
-
 }
